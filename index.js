424c507adff32bb402991b7b4698e11682b0c32d
takuya0206 <tokiwatakuya@gmail.com>
2020-07-17 22:32:56 +0900

    各ファイルへ変換か、1枚に統合か

--- index.js
+++ index.js
@@ -3,17 +3,49 @@ const path = require('path');
 const imagesToPdf = require("images-to-pdf");
 const imgDir = './img';
 const pdfDir = './pdf';
+let margeType = '';
 
 
-const converImgToPdf = () => {
-  const imgNameList = fs.readdirSync(imgDir);
-  for (let i = 0; i < imgNameList.length; i++) {
-    if (/\.png/.test(imgNameList[i])) {
-      const imgPath = path.join(imgDir, imgNameList[i]);
-      const pdfPath = path.join(pdfDir, imgNameList[i].split('.png')[0]+'.pdf');
-      imagesToPdf([imgPath], pdfPath);
+if (process.argv.length > 2) {
+    switch (process.argv[2]) {
+        case "each":
+            margeType = 'each';
+            break;
+        case "all":
+            margeType = 'all';
+            break;
+        default:
+            console.error("index.js [ each | all ] Invalid arg => " + process.argv[2])
+            process.exit(-1)
     }
+} else {
+    margeType = 'each';
+}
+
+
+
+const converImgToPdf = (margeType) => {
+  const imgNameList = fs.readdirSync(imgDir);
+
+  if (margeType === 'each') {
+    imgNameList.forEach((fileName) => {
+      if (/\.png/.test(fileName)){
+        const imgPath = path.join(imgDir, fileName);
+        const pdfPath = path.join(pdfDir, fileName.split('.png')[0]+'.pdf');
+        imagesToPdf([imgPath], pdfPath);
+      }
+    })
+  }
+
+  if (margeType === 'all') {
+    const imgPathList = imgNameList.map((fileName) => {
+      if (/\.png/.test(fileName)){
+        return path.join(imgDir, fileName)
+      }
+    })
+    const pdfPath = path.join(pdfDir, 'convertedPDF.pdf');
+    imagesToPdf(imgPathList, pdfPath);
   }
 }
 
-converImgToPdf();
+converImgToPdf(margeType);
